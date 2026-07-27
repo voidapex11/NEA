ed45ef9ee6114a05c3ce966c656ae54fa8232d10
Samuel Gibbs <154328367+voidapex11@users.noreply.github.com>
2026-07-27 18:09:06 +0100

    textbox class created, but not connected to program

--- python/nea/ui.py
+++ python/nea/ui.py
@@ -43,3 +43,41 @@ class Button:
                 if self.clicked_on():
                         for hook, args in self.hooks["mouse"]:
                                 hook(*(args[0]))
+
+class TextBox:
+        def __init__(self, position: pygame.Rect, text=""):
+                self.text = text
+                self.active = False
+                self.outer_rect = position
+                self.width = position.width
+                self.text_rect = position.move(BORDER_HEIGHT,BORDER_HEIGHT)
+                self.text_rect.clip(position)
+
+        def draw(self, surface):
+                if self.active:
+                        self.colour = DARK_GREY
+                else:
+                        self.colour = LIGHT_GREY
+                
+                pygame.draw.rect(surface,self.colour,self.outer_rect)
+
+                font = pygame.font.Font(None, FONT_HEIGHT)
+                text = font.render(self.text,True,(0,0,0))
+                surface.blit(
+                        text,
+                        self.text_rect
+                        )
+                self.outer_rect.w = max(self.width,text.get_width()+BORDER_HEIGHT)
+
+        def tick(self):
+                for event in pygame.event.get():
+                        if event.type == pygame.MOUSEBUTTONDOWN:
+                                if self.outer_rect.collidepoint(event.pos):
+                                        self.active = True
+                                else:
+                                        self.active = False
+                        elif event.type == pygame.KEYDOWN:
+                                if event.key == pygame.K_BACKSPACE:
+                                        self.text = self.text[:-1]
+                                else:
+                                        self.text+=event.unicode
